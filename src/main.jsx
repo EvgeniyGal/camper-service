cdfbc50268c498c42c36ce608ed5b5a19b15bf09
Evgeniy <egeniygal@gmail.com>
2024-05-04 11:02:48 +0300

    Add githut page deployment

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/camper-service">
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           <App />
